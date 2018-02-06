a50eba9a033f5cd3533432cad88b521c366c3c29
qikong233 <249414764@qq.com>
2018-02-06 11:57:46 +0800

    Update richText.js

--- richText.js
+++ richText.js
@@ -1,28 +1,22 @@
+/*
+* created by qikong233 
+* 2018-02-01
+*/
 import React, { Component } from 'react';
 import {
   Text,
-  Dimensions,
   Image,
-  StyleSheet,
+  PixelRatio,
+  Platform,
 } from 'react-native';
-
-const styles = StyleSheet.create({
-  container: {
-    flexWrap: 'wrap',
-    flexDirection: 'row',
-  },
-});
-
-const { width } = Dimensions.get('window');
 
 export default class extends Component {
 
   static defaultProps = {
     emoticonReg: '\\[[^\\]]+\\]',
-    textWidth: 0,
-    letterWidth: 0,
-    chineseCharacterWidth: 0,
-    punctuationWidth: 0,
+    fontSize: 14,
+    emojiSize: 15,
+    emojiReflection: {},
   };
 
   constructor(props) {
@@ -31,6 +25,11 @@ export default class extends Component {
       views: [],
     };
     this.finish = false;
+    this.emojiMap = new Map();
+    this.emoticonScale = Platform.OS === 'ios' ? 1 : PixelRatio.get();
+    if (props.emojiReflection) {
+      props.emojiReflection.map(obj => this.emojiMap.set(Object.keys(obj)[0], Object.values(obj)[0]));
+    }
   }
 
   matchContentString = (textContent) => {
@@ -63,10 +62,14 @@ export default class extends Component {
     emoticonAt = new RegExp(this.props.emoticonReg);
     const matchStr = emoticonStr.match(emoticonAt);
     const emoticonLength = matchStr[0].length;
+    emoticonKey = emoticonStr.substring(0, emoticonLength);
     const imageView = (<Image
       key={emoticonStr}
-      source={require('../../icons/emoticon/emoji-1.png')}
-      style={[{width: 20, height: 20, backgroundColor: 'green'}, this.props.emojiStyle]}
+      source={this.emojiMap.get(emoticonKey)}
+      style={[{
+        width: this.props.emojiSize * this.emoticonScale,
+        height: this.props.emojiSize * this.emoticonScale,
+      }, this.props.emojiStyle]}
     />);
     arr = this.state.views;
     arr.push(imageView);
@@ -77,7 +80,12 @@ export default class extends Component {
   render() {
     !this.finish && this.matchContentString(this.props.children);
     return (
-      <Text style={this.props.style} >
+      <Text style={[this.props.style,
+        {
+        fontSize: this.props.fontSize,
+        textAlignVertical: 'center',
+        }]}
+      >
         {this.state.views}
       </Text>
     );
